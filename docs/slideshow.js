3ff05edd0f6a562b1fdb8e26d7ac686b25626db1
ThomasS <thomass@yard.de>
2022-11-03 14:05:11 +0100

    slideshow fixed

--- docs/slideshow.js
+++ docs/slideshow.js
@@ -17,23 +17,32 @@ function updateSlideShow() {
 
 function getNextImage(lastImageName) {
     if (lastImageName == null) {
-        return currentAlbum.chapter[0].images[0];
+        return currentAlbum.chapter[0].elements[0].photo;
     }
     for (var cidx = 0; cidx < currentAlbum.chapter.length; cidx++) {
         var chapter = currentAlbum.chapter[cidx];
-        let index = chapter.images.indexOf(lastImageName);
+        let index = getIndexOfImage(lastImageName, chapter.elements);
         if (index >= 0) {
             // last image found
-            if (index < chapter.images.length - 1) {
-                return chapter.images[index+1];
+            if (index < chapter.elements.length - 1) {
+                return chapter.elements[index+1].photo;
             }
             // step to next chapter
             if (cidx < currentAlbum.chapter.length - 1) {
-                return currentAlbum.chapter[cidx+1].images[0];
+                return currentAlbum.chapter[cidx+1].elements[0].photo;
             }
             // back to beginning
-            return currentAlbum.chapter[0].images[0];
+            return currentAlbum.chapter[0].elements[0].photo;
         }
     }
     console.log("next image not found for ", lastImageName);
+}
+
+function getIndexOfImage(imageName, elements) {
+    for (var index = 0; index < elements.length; index++) {
+        if (elements[index].photo == imageName) {
+            return index;
+        }
+    }
+    return -1;
 }
